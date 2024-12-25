a3fd024252cc63e99b2e4656c45de1c899a764da
shuoga <shuoga0426@gmail.com>
2024-12-25 20:11:31 +0900

    Initial commit

---
+++ src/main/java/net/shuoga/tutorialfabric/TutorialFabric121xDataGenerator.java
@@ -0,0 +1,11 @@
+package net.shuoga.tutorialfabric;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TutorialFabric121xDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/net/shuoga/tutorialfabric/TutorialFabricClient.java
@@ -0,0 +1,11 @@
+package net.shuoga.tutorialfabric;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialFabricClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
