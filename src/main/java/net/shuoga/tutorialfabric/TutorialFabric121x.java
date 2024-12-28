9619d1cf3c170e83285b3f59816d4781d647e4ae
shuoga <shuoga0426@gmail.com>
2024-12-28 19:37:45 +0900

    pink garnet is now void

--- src/main/java/net/shuoga/tutorialfabric/TutorialFabric121x.java
+++ src/main/java/net/shuoga/tutorialfabric/TutorialFabric121x.java
@@ -7,11 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialFabric121x implements ModInitializer {
-	public static final String MOD_ID = "tutorialShuogaMod";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final String MOD_ID = "tutorialshuogamod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
--- src/main/java/net/shuoga/tutorialfabric/item/ModItems.java
+++ src/main/java/net/shuoga/tutorialfabric/item/ModItems.java
@@ -1,6 +1,8 @@
 package net.shuoga.tutorialfabric.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -8,13 +10,21 @@ import net.shuoga.tutorialfabric.TutorialFabric121x;
 
 public class ModItems {
 
-    public static  final Item PINK_GARNET = registerItem("pink_garnet",new Item(new Item.Settings()));
+    // この一文で、アイテムがゲームに追加される。ただし、名前以外虚無
+    // ここの名前が、 langディレクトリでの名前指定になって、lang内で、ゲーム内名前を決める
+    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
 
-    private static Item registerItem(String name,Item item){
-        return Registry.register(Registries.ITEM, Identifier.of(TutorialFabric121x.MOD_ID,name),item);
+    // メソッド抽出
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(TutorialFabric121x.MOD_ID, name), item);
     }
 
-    public static void registerModItems(){
+    public static void registerModItems() {
+        // 初期化時に実行される
         TutorialFabric121x.LOGGER.info("registering Mod items for " + TutorialFabric121x.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(PINK_GARNET);
+        });
     }
 }
